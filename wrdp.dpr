2cdf53d11968ca82ad016cd3d006ac446a87c47d
tuko <tuko.ustx@gmail.com>
2020-05-14 21:39:22 -0700

    Init

---
+++ wrdp.dpr
@@ -0,0 +1,14 @@
+program wrdp;
+
+uses
+  Vcl.Forms,
+  main in 'main.pas' {FormMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.Run;
+end.
