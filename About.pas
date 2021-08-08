32835243cb639d8df8367dfff75b2ab2a65eceaa
tuko <tuko.ustx@gmail.com>
2021-08-08 00:56:11 -0500

    Show version in About dialog

--- About.pas
+++ About.pas
@@ -27,6 +27,28 @@ implementation
 
 {$R *.dfm}
 
+function GetFileVersion(exeName : string): string;
+const
+  c_StringInfo = 'StringFileInfo\040904E4\FileVersion';
+var
+  n, Len : cardinal;
+  Buf, Value : PChar;
+begin
+  Result := '';
+  n := GetFileVersionInfoSize(PChar(exeName),n);
+  if n > 0 then begin
+    Buf := AllocMem(n);
+    try
+      GetFileVersionInfo(PChar(exeName),0,n,Buf);
+      if VerQueryValue(Buf,PChar(c_StringInfo),Pointer(Value),Len) then begin
+        Result := Trim(Value);
+      end;
+    finally
+      FreeMem(Buf,n);
+    end;
+  end;
+end;
+
 procedure TFormAbout.FormClose(Sender: TObject; var Action: TCloseAction);
 var
   Ini : TIniFile;
@@ -35,6 +57,7 @@ begin
   try
     Ini.WriteInteger('FormAbout','Top', self.Top);
     Ini.WriteInteger('FormAbout','Left', self.Left);
+    Ini.WriteInteger('FormAbout','Width', self.Width);
   finally
      Ini.Free;
   end;
@@ -48,9 +71,12 @@ begin
   try
     self.Top := Ini.ReadInteger( 'FormAbout', 'Top', 0);
     self.Left := Ini.ReadInteger( 'FormAbout', 'Left', 0);
+    self.Width := Ini.ReadInteger( 'FormAbout', 'Width', 300);
   finally
     Ini.Free;
   end;
+
+  self.Caption := self.Caption + ' v:' + GetFileVersion(ParamStr(0));
 end;
 
 end.
